4017f1ebd924f2027b3da8e5cea3e46ca193972a
zcaspall <55821382+zcaspall@users.noreply.github.com>
2021-09-12 20:17:02 -0500

    started on header

---
+++ frontend/src/components/header/header.js
@@ -0,0 +1,15 @@
+import * as React from 'react'
+import Logo from '../../images/Logo.svg'
+
+const header = () => {
+    <header className="header-container">
+        <div className="logo-container">
+            <img src={Logo} alt="Logo" />
+        </div>
+        <nav className="main-nav">
+            
+        </nav>
+    </header>
+}
+
+export default header
